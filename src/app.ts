9e1470ab9d90ce38c4a7b9090329846b43cc15c0
Leoneider <leoneidertrigos@wposs.com>
2021-07-15 10:24:39 -0500

    se desinstala cors

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,8 @@
 import express from 'express';
 import { Errors } from './helpers/errors.helper';
 import { UsuarioRouter } from './module/infraestructure/usuario-router';
-import cors from 'cors';
 
 const app = express();
-
-const corsOptions = {
-    origin: '*',
-    allowedHeaders: ['Authorization','Content-Type'],
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true
-  }
-
-
-app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
